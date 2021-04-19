c854766062415c1aace97548402b1cdcb60e5168
Nathan <1nathandunne1@gmail.com>
2021-04-19 01:45:05 +0100

    Removed same repeated texture creation function.

--- drawing/sprite_renderer.go
+++ drawing/sprite_renderer.go
@@ -16,7 +16,10 @@ type SpriteRenderer struct {
 
 // Return a POINTER to a spriteRenderer. Pointers to components and elements. Passing these around and need to make sure always refering to the same one.
 func NewSpriteRenderer(container *element.Element, renderer *sdl.Renderer, filename string) *SpriteRenderer {
-	tex := textureFromBMP(renderer, filename)
+	tex, err := LoadTextureFromBMP(filename, renderer)
+	if err != nil {
+		fmt.Errorf("loading textures: %v", err)
+	}
 
 	_, _, width, height, err := tex.Query()
 	if err != nil {
@@ -24,7 +27,7 @@ func NewSpriteRenderer(container *element.Element, renderer *sdl.Renderer, filen
 	}
 	return &SpriteRenderer{
 		container: container,
-		tex:       textureFromBMP(renderer, filename),
+		tex:       tex,
 		Width:     float64(width),
 		Height:    float64(height),
 	}
@@ -40,22 +43,4 @@ func (sr *SpriteRenderer) OnUpdate() error {
 
 func (mover *SpriteRenderer) OnCollision(other *element.Element) error {
 	return nil
-}
-
-func textureFromBMP(renderer *sdl.Renderer, filename string) *sdl.Texture {
-	img, err := sdl.LoadBMP(filename)
-	if err != nil {
-		panic(fmt.Errorf("loading %v: %v", filename, err))
-	}
-	defer img.Free() // Make sure we clean up, might have memory leaks if not careful. After in texture we don't need img any more.
-
-	// Load image into ram, texture into gpu ram.
-	// := is not assignment, it is a short variable declaration. Assignment uses e.g. the simple = operator.
-	// As its name says: it is to declare variables
-	tex, err := renderer.CreateTextureFromSurface(img)
-	if err != nil {
-		panic(fmt.Errorf("creating texture from %v: %v", filename, err))
-	}
-
-	return tex
 }
