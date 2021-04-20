d487a0a80b0631f37238a07b0e0c03ae8eff73f7
Nathan <1nathandunne1@gmail.com>
2021-04-20 15:39:29 +0100

    Added tests to collisions.
    Testing perfect, non-perfect and no overlapping circle collisions.

---
+++ collisions/collisions_test.go
@@ -0,0 +1,65 @@
+package collisions
+
+import (
+	"testing"
+
+	"github.com/Nathan-Dunne/GoLayer/element"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCollisionCollidesGivenTwoPerfectlyOverlappingCircles(t *testing.T) {
+
+	circle_one := element.Circle{
+		Center: element.Vector{X: 0, Y: 0},
+		Radius: 10,
+	}
+
+	circle_two := element.Circle{
+		Center: element.Vector{X: 0, Y: 0},
+		Radius: 10,
+	}
+
+	expected := true
+	actual := collides(circle_one, circle_two)
+
+	assert.Equal(t, expected, actual)
+
+}
+
+func TestCollisionCollidesGivenTwoNonPerfectlyOverlappingCircles(t *testing.T) {
+
+	circle_one := element.Circle{
+		Center: element.Vector{X: 5, Y: 5},
+		Radius: 10,
+	}
+
+	circle_two := element.Circle{
+		Center: element.Vector{X: 0, Y: 0},
+		Radius: 10,
+	}
+
+	expected := true
+	actual := collides(circle_one, circle_two)
+
+	assert.Equal(t, expected, actual)
+
+}
+
+func TestCollisionDoesNotCollideGivenTwoNonOverlappingCircles(t *testing.T) {
+
+	circle_one := element.Circle{
+		Center: element.Vector{X: 100, Y: 100},
+		Radius: 10,
+	}
+
+	circle_two := element.Circle{
+		Center: element.Vector{X: 115, Y: 115},
+		Radius: 10,
+	}
+
+	expected := false
+	actual := collides(circle_one, circle_two)
+
+	assert.Equal(t, expected, actual)
+
+}
